73a1bca2ff1d2066c978a0d49b55d952dafb830d
Tahsin Faraz Khan <twofirstkinds@gmail.com>
2024-12-19 16:19:11 +0530

    Update default.ts

--- src/.config/default.ts
+++ src/.config/default.ts
@@ -6,7 +6,7 @@ import type { ThemeConfig } from '~/types'
 export const defaultConfig: ThemeConfig = {
   site: {
     title: 'tahsin faraz khan',
-    subtitle: 'tfk',
+    subtitle: 'cultures coincide, edges explored'
     author: 'Moeyua',
     description: 'Rediscory the beauty of typography',
     website: 'https://astro-theme-typography.vercel.app/',
