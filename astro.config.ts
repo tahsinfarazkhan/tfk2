9e567a67bf50f8e92b16f96d8056ec52552e5971
Tahsin Faraz Khan <twofirstkinds@gmail.com>
2024-12-20 16:46:28 +0530

    Update astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -3,6 +3,7 @@ import sitemap from '@astrojs/sitemap'
 import swup from '@swup/astro'
 import robotsTxt from 'astro-robots-txt'
 import { defineConfig } from 'astro/config'
+import legacy from '@vitejs/plugin-legacy';
 import rehypeKatex from 'rehype-katex'
 import remarkMath from 'remark-math'
 import UnoCSS from 'unocss/astro'
