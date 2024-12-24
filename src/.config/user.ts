82dba4655dfa1fff630a94976c9905221daf6512
Tahsin Faraz Khan <twofirstkinds@gmail.com>
2024-12-25 01:33:58 +0530

    Update user.ts

--- src/.config/user.ts
+++ src/.config/user.ts
@@ -4,4 +4,7 @@ export const userConfig: Partial<UserConfig> = {
   // Override the default config here
   // site: { title: "tahsin faraz khan"},
   // seo: { twitter: "@twofirstkinds" },
+  analytics: {
+    googleAnalyticsId: 'G-PHVKG5BBW4',
+  },
 }
